362bd32d67f2c4c5f5e1001ad279614a7b0ff2d9
ShuMAhd <shuman@zykaika.ru>
2023-04-07 15:42:51 +0800

    Добавил функцию деления на ноль

--- Lesson_1ErrorHandlingInProgramming/AppLication.java
+++ Lesson_1ErrorHandlingInProgramming/AppLication.java
@@ -1,23 +1,33 @@
 package Lesson_1ErrorHandlingInProgramming;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class AppLication {
 
   /**
    * Функция для возврата размера файла
+   *
    * @param args
    */
   public static void main(String[] args) {
     //System.out.println(getFileSize(new File("/О_IT/Project/java/Исключения в программировании и их обработка (лекции)/Exceptions-in-programming-and-their-handling-lectures-/README.md")));
+
     System.out.println(getFileSize(new File
         ("/О_IT/Project/java"
             + "/Исключения в программировании и их обработка (лекции)"
             + "/Exceptions-in-programming-and-their-handling-lectures-"
             + "/Lesson_1ErrorHandlingInProgramming/TestFile")));
+    System.out.println(divide(10, 0));
   }
+
+  //функция деления на ноль
+  public static int divide(int a, int b) {
+    return a / b;
+  }
+
   //long-так обозначается размер файла, в функцию передаём объект файл (File file)
-  public static long getFileSize(File file){
+  public static long getFileSize(File file) {
     //добавим обработчик ошибки, если файл не существует
     if (!file.exists()) {
       return -1L;
