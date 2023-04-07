71c7cc95579b4dc92baec37097f74970bda26184
ShuMAhd <shuman@zykaika.ru>
2023-04-07 15:35:48 +0800

    Добавил ссылку на файл

--- Lesson_1ErrorHandlingInProgramming/AppLication.java
+++ Lesson_1ErrorHandlingInProgramming/AppLication.java
@@ -9,8 +9,12 @@ public class AppLication {
    * @param args
    */
   public static void main(String[] args) {
-    System.out.println(getFileSize(new File("123")));
-
+    //System.out.println(getFileSize(new File("/О_IT/Project/java/Исключения в программировании и их обработка (лекции)/Exceptions-in-programming-and-their-handling-lectures-/README.md")));
+    System.out.println(getFileSize(new File
+        ("/О_IT/Project/java"
+            + "/Исключения в программировании и их обработка (лекции)"
+            + "/Exceptions-in-programming-and-their-handling-lectures-"
+            + "/Lesson_1ErrorHandlingInProgramming/TestFile")));
   }
   //long-так обозначается размер файла, в функцию передаём объект файл (File file)
   public static long getFileSize(File file){
